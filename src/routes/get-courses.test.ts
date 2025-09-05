460b9a251b38b72710491493bd6bee8c48b9012e
Bernardo <bmn02@Outlook.com>
2025-09-05 01:42:53 -0300

    feat: add Login Route and some features

--- src/routes/get-courses.test.ts
+++ src/routes/get-courses.test.ts
@@ -3,7 +3,10 @@ import { randomUUID } from "node:crypto";
 import request from "supertest";
 import { server } from "../app.ts";
 import { makeCourse } from "../tests/factories/make-course.ts";
-import { makeUser } from "../tests/factories/make-user.ts";
+import {
+  makeAuthenticatedUser,
+  makeUser,
+} from "../tests/factories/make-user.ts";
 import { makeEnrollment } from "../tests/factories/make-enrollment.ts";
 
 test("get courses", async () => {
@@ -11,11 +14,15 @@ test("get courses", async () => {
 
   const title = randomUUID();
 
+  const { token } = await makeAuthenticatedUser("manager");
+
   const course = await makeCourse(title);
-  const user = await makeUser();
+  const { user } = await makeUser();
   const enrollment = await makeEnrollment(course.id, user.id);
 
-  const response = await request(server.server).get(`/courses?search=${title}`);
+  const response = await request(server.server)
+    .get(`/courses?search=${title}`)
+    .set("Authorization", token);
 
   expect(response.status).toEqual(200);
   expect(response.body).toEqual({
